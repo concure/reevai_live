0fd6d4400748b72e4f5ab8bb38c0e26898067e66
Denis Varga <>
2019-03-23 13:22:59 +0100

    contact form error fix

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-    <script src="http://localhost:35729/livereload.js"></script>
     <?php wp_head(); ?>
 </head>
 
