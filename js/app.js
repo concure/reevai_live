c3f459b9d75cb8994791d8c7d653038ed6f86f0d
Dzulien <varsum.design@gmail.com>
2019-03-27 01:06:24 +0100

    change script reference gallery 1.3

--- js/app.js
+++ js/app.js
@@ -46,13 +46,13 @@ jQuery(document).ready(function($) {
      deleteItems.forEach((el) =>{
        el.classList.remove('box-hidden');
      });
-    } else if (arrayItems.length < 3) {
+    }/* else if (arrayItems.length < 3) {
      btn.textContent = "Zobraziť menej";
      let hideAgain = arrayGalleryItems.slice(-6);
      hideAgain.forEach((el) => {
        el.classList.add('box-hidden');
      });
-   }
+   }*/
     console.log(arrayGalleryItems);
     console.log(arrayItems);
     msnry.layout();
